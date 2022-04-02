c2dbc420daea92ee6d1c0fbd2a4f1c25d480ca58
Jelle <r0888429student.thomasmore.be>
2022-04-02 15:23:00 +0200

    Alerts tonen en verbergen

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-document.getElementById("error").style.visibility = 'hidden';
-document.getElementById("success").style.visibility = 'hidden';
-document.getElementById("info").style.visibility = 'hidden';
-
-//https://stackoverflow.com/questions/19454310/stop-form-refreshing-page-on-submit
-//let form = document.querySelector('#btnSubmit');
-//document.querySelector('#btnSubmit').addEventListener('click' , validateForm, false);
-//function validateForm(event) {event.preventDefault(); }
-//form.addEventListener('submit', validateForm);
-
+// Alerts verbergen
+hideAlerts();
 
 //https://www.youtube.com/watch?v=In0nB0ABaUk
 const validateForm = document.getElementById('form');
@@ -15,38 +7,42 @@ const validateForm = document.getElementById('form');
 let errors = [];
 
 validateForm.addEventListener('submit', (e) => {
+    // Array leegmaken
+    errors = []; 
+    // Alerts verbergen 
+    hideAlerts();
+
     // Validaties
-    // checkEmptyField('#voornaam', 'Het veld voornaam is vereist.');
-    // checkEmptyField('#naam', 'Het veld naam is vereist.');
-    // checkEmptyField('#gebruikersnaam', 'Het veld gebruikersnaam is vereist.');
-    // checkEmptyField('#email', 'Het veld email is vereist.');
-    // checkEmptyField('#wachtwoord', 'Het veld wachtwoord is vereist.');
-    // checkEmptyField('#herhaalWachtwoord', 'Het veld herhaal wachtwoord is vereist.');
-    // checkEmptyField('#adres', 'Het veld adres is vereist.');
-    // checkEmptyField('#land', 'Het veld land is vereist.');
-    // checkEmptyField('#provincie', 'Het veld provincie is vereist.');
-    // checkEmptyField('#postcode', 'Het veld postcode is vereist.');
+    checkEmptyField('#voornaam', 'Het veld voornaam is vereist.');
+    checkEmptyField('#naam', 'Het veld naam is vereist.');
+    checkEmptyField('#gebruikersnaam', 'Het veld gebruikersnaam is vereist.');
+    checkEmptyField('#email', 'Het veld email is vereist.');
+    checkEmptyField('#wachtwoord', 'Het veld wachtwoord is vereist.');
+    checkEmptyField('#herhaalWachtwoord', 'Het veld herhaal wachtwoord is vereist.');
+    checkEmptyField('#adres', 'Het veld adres is vereist.');
+    checkEmptyField('#land', 'Het veld land is vereist.');
+    checkEmptyField('#provincie', 'Het veld provincie is vereist.');
+    checkEmptyField('#postcode', 'Het veld postcode is vereist.');
 
     //Check e-mailadres
-    //validateEmail('#email');
+    validateEmail('#email');
     
     //Check wachtwoorden
-    // validatePassword("wachtwoord", "herhaalWachtwoord");
+    validatePassword("wachtwoord", "herhaalWachtwoord");
+
+    //Check postcode
+    checkPC('#postcode');
 
     //Check betalingswijze
     let betalingswijze = validatePayment('input[name="flexRadioDefault"]:checked');
 
-    //Check postcode
-    // checkPC('#postcode');
-
     // Controleren of algemene voorwaarden zijn aangeduid.
-    //algemeneVoorwaarden('#algemeneVoorwaarden', 'Je moet de algemene voorwaarden accepteren.');
+    algemeneVoorwaarden('#algemeneVoorwaarden', 'Je moet de algemene voorwaarden accepteren.');
 
-    // Meldingen tonen
+    // Alerts tonen
     if (errors.length > 0){
         document.getElementById("error").style.visibility = 'visible';
         //https://stackoverflow.com/questions/28600453/add-text-to-a-p-tag-by-using-the-id-of-the-body-tag-in-javascript
-        //document.getElementById("error").innerHTML.P += errors;
         document.getElementsByTagName("p")[0].innerText = errors;  
     } 
     else 
@@ -58,6 +54,12 @@ validateForm.addEventListener('submit', (e) => {
     e.preventDefault();
 })
 
+// Functie om de alerts te verbergen
+function hideAlerts(){
+    document.getElementById("error").style.visibility = 'hidden';
+    document.getElementById("success").style.visibility = 'hidden';
+    document.getElementById("info").style.visibility = 'hidden';
+}
 
 // Functie controle lege velden
 function checkEmptyField(veld, melding){
@@ -66,7 +68,6 @@ function checkEmptyField(veld, melding){
         errors += melding + "\n";
     } 
 }
-
 
 // Functie controle e-mailadres
 function validateEmail(email){
@@ -83,7 +84,6 @@ function validateEmail(email){
       return false;
     }
 }
-
 
 // Functie controle wachtwoord
 function validatePassword(password, repeatPassword){
@@ -103,13 +103,11 @@ function validatePassword(password, repeatPassword){
     }  
 }
 
-
 // Functie controle betalingswijze
 function validatePayment(veld){
     let input = document.querySelector(veld).value;
     return input;
 }
-
 
 // Functie controle postcode
 function checkPC(veld){
